757e26fab00987a8d23ef2a9f5a04797145428bd
usam-vijay <usamvijay@gmail.com>
2023-03-26 12:45:14 +0530

    add Product slider and Footer section

--- js/script.js
+++ js/script.js
@@ -14,3 +14,26 @@ function showSlider(){
     slides[slideindex-1].style.display="block";
     setTimeout(showSlider, 3000);
 }
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayHoverPause:true,
+    margin:10,
+   
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
